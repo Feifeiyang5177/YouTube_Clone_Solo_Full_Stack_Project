9963f0cfe9299fe002cb5b81d41aa7597210d4ff
Feifei Yang <feifeiyang@feifeis-air-2.home>
2021-11-02 15:49:06 -0400

    end

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,28 +1,29 @@
 class SessionsController < ApplicationController
 
-    def create 
-        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
-        if @user.nil?
-          render json: ["Invalid username or password"], status: 401
-        else
-          login!(@user)
-          render 'api/users/show'
-        end
-      end
+  def create 
 
-      def destroy
-        @user = current_user
-        if @user
-          logout
-          render "api/users/show"
-        else
-          render json: ["No current user"], status: 404
-        end
-      end
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user.nil?
+      render json: ["Invalid username or password"], status: 401
+    else
+      login!(@user)
+      render 'api/users/show'
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["No current user"], status: 404
+    end
+  end
     
       # def destroy
       #   logout!
       #   render json: ['Logout successful.' ]
       # end
-    end
+
 end
